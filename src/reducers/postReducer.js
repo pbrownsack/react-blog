85d8d2a7bb3f44d4d8da4fefe7c7a5fdb75948fd
pbrownsack <pqtbrown@gmail.com>
2019-05-07 10:09:19 -0500

    Add react-router stuff

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -3,15 +3,17 @@ import { FETCH_POSTS, FETCH_POST } from '../actions/types';
 const postReducer = (state = [], action) => {
     switch (action.type) {
         case FETCH_POSTS: {
-
+            break;
         }
 
         case FETCH_POST: {
+            break;
+        }
 
+        default: {
+            return state;
         }
     }
-
-    return state;
 }
 
 export default postReducer;
