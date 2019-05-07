45cdab42332aaf76347b1764fd19c157cfab8028
pbrownsack <pqtbrown@gmail.com>
2019-05-07 09:56:27 -0500

    Add React boilerplate

---
+++ src/actions/types.js
@@ -0,0 +1,3 @@
+export const FETCH_POSTS = "FETCH_POSTS";
+export const FETCH_POST = "FETCH_POST_SINGLE";
+export const FETCH_USER = "FETCH_USER";
---
+++ src/reducers/postReducer.js
@@ -0,0 +1,17 @@
+import { FETCH_POSTS, FETCH_POST } from '../actions/types';
+
+const postReducer = (state = [], action) => {
+    switch (action.type) {
+        case FETCH_POSTS: {
+
+        }
+
+        case FETCH_POST: {
+
+        }
+    }
+
+    return state;
+}
+
+export default postReducer;
