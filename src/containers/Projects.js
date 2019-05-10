197fdeb3caa5f6dda2a8c8585c72440ee229ed19
pbrownsack <pqtbrown@gmail.com>
2019-05-09 22:08:47 -0500

    Revise page headings

--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import Header, { HeaderText } from '../components/Header';
 import Card, { CardInfo } from '../components/Card';
 import './Projects.scss';
 import logo from '../img/chloride_logo.png';
+import git from '../img/social/Github.svg';
+
+const linkStyles = {
+    width: '48px',
+    height: '48px'
+}
 
 const Projects = () => {
     return (
         <div className="projects">
-            <Header>
-                <i class="fas fa-book"></i>
-                <HeaderText>
-                    <h2>My personal projects, showcased for you.</h2>
-                    <p>All of my personal projects including code, music, and other cool stuff will be here.</p>
-                </HeaderText>
-            </Header>
+            <h1 className="head">Personal Projects</h1>
+            <p className="sub-head">A list of my dearest personal projects.</p>
             <hr />
             <Card>
                 <img src={logo} />
@@ -21,6 +21,7 @@ const Projects = () => {
                     <h4>This website</h4>
                     <p>This website is an ongoing project created with ReactJS, Express, and SCSS.</p>
                 </CardInfo>
+                <a href="https://github.com/pbrownsack/react-blog" style={{marginLeft: 'auto'}}><img src={git} style={linkStyles} /></a>
             </Card>
             <Card>
                 <img src={logo} />
