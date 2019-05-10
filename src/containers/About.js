41f7923e3f41045ab2c7e9d5b536429abef65a3d
pbrownsack <pqtbrown@gmail.com>
2019-05-09 22:09:09 -0500

    Add about page

---
+++ src/containers/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './About.scss';
+
+const About = () => {
+    return (
+        <div className="about">
+            <h1>About Me</h1>
+            <p className="sub-head">A little about me, Parker Brown.</p>
+            <hr />
+
+            <h2 style={{color: 'rgb(80, 80, 80)'}}>Turtles are really cool!</h2>
+        </div>
+    )
+}
+
+export default About;
