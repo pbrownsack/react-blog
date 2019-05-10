e12c606436ca4f491da4b20ba1bed3d30f441a7b
pbrownsack <pqtbrown@gmail.com>
2019-05-09 22:09:01 -0500

    Remove login heading

--- src/containers/Admin.js
+++ src/containers/Admin.js
@@ -4,8 +4,6 @@ import './Admin.scss';
 const Admin = (props) => {
     return (
         <div className="admin">
-            <h3>Admin Panel</h3>
-            <hr />
             <input type="text" placeholder="Username" />
             <input type="password" placeholder="Password" />
             <input type="submit" value="Login" />
